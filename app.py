b85f5bc0805e97a777933e2c58edd1c91189eb14
Olivia  Tierney <oliviatierney@Olivias-MacBook-Pro.local>
2021-09-18 10:19:37 -0400

    update and delete function

--- app.py
+++ app.py
@@ -27,6 +27,19 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+@app.route("/update/<int:todo_id>")
+def update(todo_id): 
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete 
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id): 
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__": 
     db.create_all()
